dfdb2d7b94beb824607d17a102e9e887d6a2f50c
trickedparsley <trickedparsley@gmail.com>
2026-03-27 20:52:21 +0200

    first commit of asteroids project

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import *
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+
+
+if __name__ == "__main__":
+    main()
