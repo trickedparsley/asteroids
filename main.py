2cb0875bde661725249fbcc15598f566a3986603
trickedparsley <trickedparsley@gmail.com>
2026-03-28 20:43:24 +0200

    Ticks and delta time implemented

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
