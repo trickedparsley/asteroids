6ff106ce70d7a53fc6a029f88f9ef8c1c385cf05
trickedparsley <trickedparsley@gmail.com>
2026-08-09 16:05:18 +0300

    Player projectiles now destroy asteroids on collision

--- main.py
+++ main.py
@@ -35,6 +35,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         screen.fill("black")
         for thing in drawable:
             thing.draw(screen)
